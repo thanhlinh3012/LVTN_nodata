3fb192988f9fe1a62ab23c2f2fe939579739f937
LinhThaiThiThanh <43703204+thanhlinh3012@users.noreply.github.com>
2021-01-02 10:27:40 +0700

    Add files via upload

--- index.py
+++ index.py
@@ -6,17 +6,17 @@ from colordescriptor import ColorDescriptor
 import argparse
 import glob
 import cv2
+import time
 
+start_time = time.time()
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required = True,
-	help = "Path to the directory that contains the images to be indexed")
-ap.add_argument("-i", "--index", required = True,
-	help = "Path to where the computed index will be stored")
+ap.add_argument("-d", "--dataset", required=True)
+ap.add_argument("-i", "--index", required=True)
 args = vars(ap.parse_args())
 # initialize the color descriptor
-cd = ColorDescriptor((8, 12, 3))
-
+#cd = ColorDescriptor((8, 12, 3))
+cd = ColorDescriptor((24, 6, 6))  # 24bins for Hue chanel, 6bins for saturation chanel, 6bins for value chanel
 # open the output index file for writing
 output = open(args["index"], "w")
 # use glob to grab the image paths and loop over them
@@ -33,3 +33,4 @@ for imagePath in glob.glob(args["dataset"] + "/*.jpg"):
 	#cv2.waitKey(1000)
 # close the index file
 output.close()
+print("%s seconds" % (time.time()-start_time))
