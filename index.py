793cb5e9bfb6c035b4fe8f5b1ffdd2f44b04a967
LinhThaiThiThanh <43703204+thanhlinh3012@users.noreply.github.com>
2021-01-12 09:27:45 +0700

    Update index.py

--- index.py
+++ index.py
@@ -7,19 +7,19 @@ import sys
 sys.path.append(".")
 
 start_time = time.time()
-# construct the argument parser and parse the arguments
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True)
 ap.add_argument("-i", "--index", required=True)
 args = vars(ap.parse_args())
+
 # initialize the color descriptor
-cd = ColorDescriptor((8, 12, 3))
-#cd = ColorDescriptor((24, 6, 6))  # 24bins for Hue chanel, 6bins for saturation chanel, 6bins for value chanel
+cd = ColorDescriptor((8, 12, 3)) # 8bins for Hue chanel, 12bins for saturation chanel, 3bins for value chanel
+ 
 # open the output index file for writing
 output = open(args["index"], "w")
-# use glob to grab the image paths and loop over them
 for imagePath in glob.glob(args["dataset"] + "/*"):
-    # extract the image ID (i.e. the unique filename) from the image
+    # extract the image ID from the image
 	# path and load the image itself
 	imageID = imagePath[imagePath.rfind("/") + 1:]
 	image = cv2.imread(imagePath)
