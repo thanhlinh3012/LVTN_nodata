eff1539eb9eea9d0aae72ba8e70fa8886c50c715
LinhThaiThiThanh <43703204+thanhlinh3012@users.noreply.github.com>
2021-01-12 09:38:14 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
-from colordescriptor import ColorDescriptor
 import argparse
 import glob
 import cv2
 import time
 import sys
+from colordescriptor import ColorDescriptor
 sys.path.append(".")
 
 start_time = time.time()
@@ -12,16 +12,15 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True)
 ap.add_argument("-i", "--index", required=True)
 args = vars(ap.parse_args())
-
 # initialize the color descriptor
-cd = ColorDescriptor((8, 12, 3)) # 8bins for Hue chanel, 12bins for saturation chanel, 3bins for value chanel
- 
+cd = ColorDescriptor((8, 12, 3))  # 8bins for Hue chanel, 12bins for saturation chanel, 3bins for value chanel
+
 # open the output index file for writing
 output = open(args["index"], "w")
 for imagePath in glob.glob(args["dataset"] + "/*"):
-    # extract the image ID from the image
-	# path and load the image itself
+	# extract the image ID from the image
 	imageID = imagePath[imagePath.rfind("/") + 1:]
+	# path and load the image itself
 	image = cv2.imread(imagePath)
 	# describe the image
 	features = cd.describe(image)
@@ -32,3 +31,5 @@ for imagePath in glob.glob(args["dataset"] + "/*"):
 # close the index file
 output.close()
 print("%s seconds" % (time.time()-start_time))
+
+
