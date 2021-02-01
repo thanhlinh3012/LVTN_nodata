0b4a06bc395835d047a8b0023967722ce8c8eef0
LinhThaiThiThanh <43703204+thanhlinh3012@users.noreply.github.com>
2021-02-01 14:51:23 +0700

    Update colordescriptor.py

--- colordescriptor.py
+++ colordescriptor.py
@@ -17,7 +17,7 @@ class ColorDescriptor:
         segments = [(0, cX, 0, cY), (cX, w, 0, cY), (cX, w, cY, h), (0, cX, cY, h)]
 
         # construct an elliptical mask representing the center of the img
-        (axesX, axesY) = (int(w * 0.75) // 2, int(h * 0.75) // 2)      # "//': chia lam tron xuong
+        (axesX, axesY) = (int(w * 0.75) // 2, int(h * 0.75) // 2)   
         ellipMask = np.zeros(image.shape[:2], dtype="uint8")
         cv2.ellipse(ellipMask, (cX, cY), (axesX, axesY), 0, 0, 360, 255, -1)
 
@@ -32,13 +32,7 @@ class ColorDescriptor:
          # extract a color histogram from the elliptical region
         hist = self.histogram(image, ellipMask)
         features.extend(hist)       # update the feature vector
-        """
-        for (startX, endX, startY, endY) in segments:
-            cornerMask = np.zeros(image.shape[:2], dtype="uint8")
-            cv2.rectangle(cornerMask, (startX, startY), (endX, endY), 255, -1)
-            hist = self.histogram(image, cornerMask)
-            features.extend(hist)
-        """
+   
         return features
 
     def histogram(self, image, mask):
